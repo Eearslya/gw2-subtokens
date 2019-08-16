14ed4a0f657e6aa987e97a0631aa44f461c85e89
Eearslya Sleiarion <eearslya@gmail.com>
2019-08-16 13:27:32 -0700

    :sparkles: Add collapsible groups to URL selector

--- app/controllers/subtokens_controller.rb
+++ app/controllers/subtokens_controller.rb
@@ -17,8 +17,34 @@ class SubtokensController < ApplicationController
   ].freeze
 
   KNOWN_ENDPOINTS = %w[
+    /v2/account
+    /v2/account/achievements
     /v2/account/bank
+    /v2/account/dailycrafting
+    /v2/account/dungeons
+    /v2/account/dyes
+    /v2/account/finishers
+    /v2/account/gliders
+    /v2/account/home/cats
+    /v2/account/home/nodes
+    /v2/account/inventory
+    /v2/account/luck
+    /v2/account/mailcarriers
+    /v2/account/mapchests
+    /v2/account/masteries
+    /v2/account/mastery/points
     /v2/account/materials
+    /v2/account/minis
+    /v2/account/mounts/skins
+    /v2/account/mounts/types
+    /v2/account/novelties
+    /v2/account/outfits
+    /v2/account/pvp/heroes
+    /v2/account/raids
+    /v2/account/recipes
+    /v2/account/titles
+    /v2/account/wallet
+    /v2/account/worldbosses
   ].freeze
 
   def new
